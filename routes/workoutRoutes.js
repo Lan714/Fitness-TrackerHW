01d958ee3a7b5b9082f90aabd207f7543ae3bf47
Lan714 <87465185+Lan714@users.noreply.github.com>
2021-10-23 00:27:06 -0700

    workoutRoutes updated

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -20,3 +20,19 @@ router.get('/workouts/range', async function (req, res) {
   res.json(workouts)
 })
 
+router.post('/workouts', async function (req, res) {
+  const workout = await Workout.create({ ..req.body })
+  res.json(workout)
+})
+
+router.put('/workouts/:id', async function (req, res) {
+  await Workout.findByIdAndUpdate(req.params.id, { $set: req.body })
+  res.sendStatus(200)
+})
+
+router.delete('/workouts/:id', asyn function (req, res) {
+  await Workout.findByIdAndDelete(req.params.id)
+  res.sendStatus(200)
+})
+
+module.exports = router
